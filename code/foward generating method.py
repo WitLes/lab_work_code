54263e662004e3d85e86b8a95e63c0a3d3ff08bd
Yuan Gao <yuan-g@foxmail.com>
2017-09-28 18:21:44 +0800

    v1.2

--- code/foward generating method.py
+++ code/foward generating method.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from random import randint 
 import numpy as np
 
-def topology_generating(initial_graph):
+def topology_generating(initial_graph):   # 生成拓扑结构
 	try:
 		G.add_nodes_from([i for i in range(0,20)])
 		G.add_weighted_edges_from([[0,1,0.0],[0,2,0.0],[0,3,0.0],[0,4,0.0]])
@@ -15,7 +15,7 @@ def topology_generating(initial_graph):
 		G.add_weighted_edges_from([[6,9,0],[6,10,0],[6,11,0],[6,12,0],[6,13,0],[6,14,0],[6,15,0]])
 		G.add_weighted_edges_from([[7,9,0],[7,10,0],[7,11,0],[7,12,0],[7,13,0],[7,14,0],[7,15,0]])
 		G.add_weighted_edges_from([[8,9,0],[8,10,0],[8,11,0],[8,12,0],[8,13,0],[8,14,0],[8,15,0]])
-		G.add_weighted_edges_from([[9,16,0],[9,17,0],[9,18,0],[9,19,0],[9,20,0]])
+		G.add_weighted_edges_from([[9,16,0],[9,17,0],[9,18,0],[9,19,0]])
 		G.add_weighted_edges_from([[10,16,0],[10,17,0],[10,18,0],[10,19,0]])
 		G.add_weighted_edges_from([[11,16,0],[11,17,0],[11,18,0],[11,19,0]])
 		G.add_weighted_edges_from([[12,16,0],[12,17,0],[12,18,0],[12,19,0]])
@@ -26,16 +26,19 @@ def topology_generating(initial_graph):
 		return 0
 	return 1
 
+# 确定每个节点在图像中的位置
+pos1 = {0:[1,3.5],1:[2,2],2:[2,3],3:[2,4],4:[2,5],5:[3,2],6:[3,2.7],7:[3,3.6],8:[3,4.4],9:[4,1],10:[4,2],11:[4,3],12:[4,4],13:[4,5],14:[4,6],15:[4,7],16:[5,1],17:[5,2],18:[5,3],19:[5,4],20:[5,5]}
 
-
-
+# 生成有向图
 G=nx.DiGraph()  
 topology_generating(G)
-infected_flag = [0 for i in range(0,20)]
+# infected_flag = [0 for i in range(0,20)]
 
+# 每个连边上都取gamma分布的抽样
 for i,j,k in G.edges(data=True):
 	G.add_weighted_edges_from([[i,j,np.random.gamma(2,3,1)[0]]])
 
+# 计算单元最短路径和路径长度
 DAT_PATH = nx.shortest_path(G,source=0,weight="weight")
 DAT = nx.shortest_path_length(G,source=0,weight="weight")
 print(DAT)
@@ -44,18 +47,20 @@ print(DAT_PATH)
 for (i,j,k) in G.edges(data=True):
 	print(i,"->",j,": ",k)
 """	
-#nx.draw(G,pos=nx.spring_layout(G),with_labels=True,node_color='red')#按参数构图  
-#plt.axis('off')
-#plt.show()#显示图像
 
 
+nx.draw(G,pos=pos1,with_labels=True,node_color='red')#按参数构图  
+plt.axis('off')
+plt.show()#显示图像
+
+# 扩散过程的可视化
 Diffusion_Tree = nx.DiGraph()
 for node,value in DAT_PATH.items():
 	print(value)
 	for i in range(0,len(value)-1):
 		Diffusion_Tree.add_weighted_edges_from([[value[i],value[i+1],0]])
 
-nx.draw(Diffusion_Tree,pos=nx.spring_layout(Diffusion_Tree),with_labels=True,node_color='blue')#按参数构图  
+nx.draw(Diffusion_Tree,pos=pos1,with_labels=True,node_color='blue')#按参数构图  
 plt.axis('off')
 plt.show()#显示图像
 
