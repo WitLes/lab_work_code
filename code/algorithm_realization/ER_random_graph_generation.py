ba1780db1905e3ec0127f49b0678d6dd1d9c8d25
Yuan Gao <yuan-g@foxmail.com>
2017-12-01 16:43:45 +0800

    v1

--- code/algorithm_realization/ER_random_graph_generation.py
+++ code/algorithm_realization/ER_random_graph_generation.py
@@ -3,6 +3,9 @@ import networkx as nx
 import random 
 import time
 import matplotlib.pyplot as plt
+import sympy as sp
+import sympy as sp
+from sympy.abc import a,x,y
 
 
 def er_graph_generator(node_number=100, link_probability=0.06,seed=None, directed=False):
@@ -30,6 +33,7 @@ def DAT_generator(graph_topology,wtd_array,diffusive_source):
 	DAT = nx.shortest_path_length(graph_topology, source=diffusive_source, weight="weight")
 	return DAT, DAT_PATH
 
+
 def dats_generator(graph_topology,dat_number=1,seed=True):
 	# This function returns the list of DATs.
 	# dat_number is the number of DAT that you want.
@@ -54,19 +58,19 @@ def dats_generator(graph_topology,dat_number=1,seed=True):
 
 
 def draw_diffusion_tree(diffusive_arrival_times, type=0):
-    Diffusion_Tree = nx.DiGraph()
-    for node, value in diffusive_arrival_times.items():
-        for i in range(0, len(value) - 1):
-            Diffusion_Tree.add_weighted_edges_from([[value[i], value[i + 1], 0]])
-    if type == 0:
-        pos = nx.spring_layout(Diffusion_Tree)
-    if type == 1:
-        pos = nx.random_layout(Diffusion_Tree)
-    if type == 2:
-        pos = nx.shell_layout(Diffusion_Tree)
-    plt.subplot(133)
-    nx.draw_networkx(Diffusion_Tree, pos, with_labels=True, node_color='blue', node_size=4)  # 按参数构图
-    return Diffusion_Tree
+	Diffusion_Tree = nx.DiGraph()
+	for node, value in diffusive_arrival_times.items():
+		for i in range(0, len(value) - 1):
+			Diffusion_Tree.add_weighted_edges_from([[value[i], value[i + 1], 0]])
+	if type == 0:
+		pos = nx.spring_layout(Diffusion_Tree)
+	if type == 1:
+		pos = nx.random_layout(Diffusion_Tree)
+	if type == 2:
+		pos = nx.shell_layout(Diffusion_Tree)
+	plt.subplot(133)
+	nx.draw_networkx(Diffusion_Tree, pos, with_labels=True, node_color='blue', node_size=4)  # 按参数构图
+	return Diffusion_Tree
 
 
 def mcmc_algorithm_with_gibbs_sampling(discrete_wtd,dats):
@@ -76,7 +80,7 @@ def mcmc_algorithm_with_gibbs_sampling(discrete_wtd,dats):
 	graph_samples = list()
 	survival_func = pdf2sf(discrete_wtd)
 	hazard_func = discrete_wtd / survival_func
-
+	lambda_dict_list = [{} for i in range(len(dats))]
 	graph = nx.Graph(directed=True)
 	iteration = 0
 	graph_sample_number = 0
@@ -90,18 +94,48 @@ def mcmc_algorithm_with_gibbs_sampling(discrete_wtd,dats):
 				pass
 		if iteration > burn_in:
 			lag += 1
-		if lag = max_lag:
-			graph_samples.append(graph)		
-			lag = 0	
+		if lag == max_lag:
+			graph_samples.append(nx.Graph(graph))
+			lag = 0
 
 	return graph_samples
 
+
 def pdf2sf():
 	pass
+
+
+def continuous_exp_distribution2discrete(delta,l_t,miu=1):
+	discrete_wtd = list()
+	discrete_mass = list()
+	discrete_wtd.append(0)
+	discrete_mass.append(0)
+	left = float(0)
+	right = float(delta)
+	for i in range(1, int(l_t/delta)):
+		temp = sp.integrate(sp.exp(-miu*x), (x,left,right))
+		discrete_mass.append(temp)
+		discrete_wtd.append(discrete_mass[i] / delta)
+		left = right
+		right += delta
+	# if there is something needed, it must be the normalization step, which makes the sum of discrete_mass equals to 1.
+
+	# discrete_wtd is a list of the discrete point of WTD.for example, list[1] = exp(delta), list[n] = exp(delta * n)
+	# discrete_mass is the list of mass of each small section.
+	return discrete_wtd, discrete_mass
+
+
+def scatter_wtd(discrete_wtd):
+	plt.scatter([i for i in range(len(discrete_wtd))],discrete_wtd)
+	plt.show()
+
+
 er_graph, node_number, edge_number = er_graph_generator(100,0.06,seed=0,directed=False)
 
 dats,dat_path = dats_generator(er_graph,dat_number=1,seed=True)
-print(er_graph.edges())
+
+discrete_wtd,discrete_mass = continuous_exp_distribution2discrete(0.01,5)
+scatter_wtd(discrete_wtd)
 
 
 
--- code/mytest.py
+++ code/mytest.py
@@ -1,9 +1,9 @@
 import numpy as np
 import networkx as nx
+import sympy as sp
+from sympy.abc import a,x,y
 
-a = []
-for i in range(10):
-	np.random.seed(i)
-	print(np.random.randint(100))
-	a.append([np.random.normal() for i in range(5)])
+b = 0
+
+a = b/2 if b!=0 else 0
 print(a)
