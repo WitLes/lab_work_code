20d3ceff61a8f4ae14cae638cccdbac4a2d15781
Yuan Gao <yuan-g@foxmail.com>
2018-05-24 10:52:42 +0800

    v3

--- code/test_file/test.py
+++ code/test_file/test.py
@@ -1,8 +1,21 @@
-from algorithm_realization.stn_reconstruction_lib import *
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
 
-demo_graph, node_number, edge_number = networkx_graph_generator(mode="ER")
-node_list = []
-for node in demo_graph.nodes():
-    node_list.append(node)
 
-print(list(sorted(node_list)))
+
+
+graph = nx.Graph()
+graph.add_nodes_from(list(range(100)))
+grid = nx.grid_2d_graph(10,10,create_using=graph)
+nx.draw(grid)
+plt.show()
+
+file = open("grid36.txt","w")
+
+for edge in grid.edges():
+    node1 = edge[0][0]*10 +edge[0][1]
+    node2 = edge[1][0]*10+edge[1][1]
+    file.write(str(node1+1)+" "+str(node2+1)+"\n")
+
+file.close()
